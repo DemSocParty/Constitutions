d0fad8ee2bfe6f1ce1d3bdb5d104a78d5cfd2ea3
insyzygy <nicholas.babcock092@gmail.com>
2016-06-10 20:41:01 -0700

    boilerplate

---
+++ app/calc.js
@@ -0,0 +1,3 @@
+var CreateCommittee = function (name, parent) {
+	
+}
---
+++ app/index.js
@@ -0,0 +1,13 @@
+// index.js
+const path = require('path')  
+const express = require('express')  
+const exphbs = require('express-handlebars')
+const fs = require('fs')
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')  
+app.set('views', path.join(__dirname, 'views'))  
